1ea786dbd78eb650ba2b0d11b140d43987cc4df2
Vashee khan <vashee.khan.lits@laxmi.edu.in>
2026-08-22 21:03:27 +0530

    fix TypeScript Express user types

--- Backend/src/types/express.d.ts
+++ Backend/src/types/express.d.ts
@@ -1,13 +1,10 @@
 import "express";
-import { Profile } from "passport-google-oauth20";
 
 declare global {
   namespace Express {
     interface Request {
       userId?: string;
     }
-
-    interface User extends Profile {}
   }
 }
 
